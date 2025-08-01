bd4eca1f92d6bef7ae5e530617f3ec7524e7d235
c4pec <162154915+Cape-c@users.noreply.github.com>
2025-07-31 21:31:28 -0300

    09-11 solved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,49 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let result = 0;
+  let largerNum = 0;
+  let shorterNum = 0;
+  let allNums = false;
 
+
+  function isInteger(a, b) {
+    let numA = Number.isInteger(a);
+    let numB = Number.isInteger(b);
+    if (numA == true && numB == true) {
+      return allNums = true;
+    };
+  }
+
+  isInteger(a, b);
+  
+  if (a > b) {
+    largerNum = a;
+    shorterNum = b;
+  } else {
+    largerNum = b;
+    shorterNum = a;
+  };
+
+  if (allNums == true && largerNum > 0 && shorterNum > 0) {
+    for (let step = shorterNum; step < largerNum; step++) {
+     result += step;
+     }
+    return result + largerNum;
+    } else {
+      return "ERROR";
+    };
 };
+/*
+Algo caotico y largo comparado a la solción :c
+Al menos si se me ocurrió:
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+...pero pensé que no era de buena practica crear una tercera variable para reorganizar.
+Esta vez si se queda la carpeta /solutions para tener en cuenta la otra solucion.
+*/
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+//divisible por 4 y 400, pero no por 100.
+  let leap = false;
+  if (year % 4 === 0) {
+    leap = true;
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        leap = true;
+      } else {
+        leap = false;
+      }
+    }
+  }
+  return leap;
 };
+// Me gustó la forma de plantearse en la solucion de TOP, se queda la carpeta!
 
 // Do not edit below this line
 module.exports = leapYears;
